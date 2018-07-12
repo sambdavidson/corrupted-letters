350ad6851a72c24fdd0f65f00bfaed8c62954810
Samuel Davidson <samdamana@gmail.com>
2018-07-11 18:23:22 -0700

    Added display for slider. Used elliot's cumulative corruption.

--- corruption.js
+++ corruption.js
@@ -1,33 +1,42 @@
-const corruptionMap = {
-    'a': ['á','ä','à','â','ā','ã'],
-    'c': ['ç','ĉ'],
-    'e': ['é','ë','è','ê','ē'],
-    'g': ['ĝ'],
-    'h': ['ĥ'],
-    'i': ['ī','î','ì','ï','í'],
-    'j': ['ĵ'],
-    'n': ['ñ'],
-    'o': ['õ','ō','ô','ò','ö','ó','ó'],
-    's': ['š','§'],
-    't': ['ť','ţ'],
-    'u': ['ū','û','ù','ü','ú'],
-    'y': ['ý','ÿ','ỳ','ŷ'],
-};
+const corruptionSets = [
+    ['a','á','ä','à','â','ā','ã'],
+    ['c','ç','ĉ'],
+    ['e','é','ë','è','ê','ē'],
+    ['g','ĝ'],
+    ['h','ĥ'],
+    ['i','ī','î','ì','ï','í'],
+    ['j','ĵ'],
+    ['n','ñ'],
+    ['o','õ','ō','ô','ò','ö','ó','ó'],
+    ['s','š','§'],
+    ['t','ť','ţ'],
+    ['u','ū','û','ù','ü','ú'],
+    ['y','ý','ÿ','ỳ','ŷ'],
+];
 
 function corruptString(str, corZeroToHundred) {
     let corruptedString = '';
 
     for (let c of str) {
-        let corruptedLetters = corruptionMap[c];
         let doCorrupt = Math.random() < (corZeroToHundred / 100);
 
-        if (!corruptedLetters || !doCorrupt) {
+        if (!doCorrupt) {
             corruptedString += c;
             continue;
         }
 
-        corruptedString += randomItem(corruptedLetters);
+        let foundMatch = false;
+        for(s of corruptionSets) {
+            if(s.includes(c)) {
+                corruptedString += randomItem(s);
+                foundMatch = true;
+                break;
+            }
+        }
 
+        if(!foundMatch) {
+            corruptedString += c;
+        }
     }
     return corruptedString;
 }
@@ -37,23 +46,43 @@ function randomItem(items) {
 }
 
 window.addEventListener('load', function () {
-    const speedInput = document.getElementById('speed');
     const corruptionInput = document.getElementById('corruption');
+    const corruptionDisplay = document.getElementById('corruption-display');
+    const speedInput = document.getElementById('speed');
+    const speedDisplay = document.getElementById('speed-display');
     const textInput = document.getElementById('text');
     const result = document.getElementById('result');
 
-    let speed = speedInput.value;
     let corruption = corruptionInput.value;
+    corruptionDisplay.innerText = corruptionInput.value;
+    let speed = speedInput.value;
+    speedDisplay.innerText = speedInput.value;
 
-    speedInput.addEventListener('input', ()=>{speed = speedInput.value;});
-    speedInput.addEventListener('change', ()=>{speed = speedInput.value;});
-    corruptionInput.addEventListener('input', ()=>{corruption = corruptionInput.value;});
-    corruptionInput.addEventListener('change', ()=>{corruption = corruptionInput.value;});
+    const corruptionChange = () => {
+    	corruption = corruptionInput.value;
+    	corruptionDisplay.innerText = corruptionInput.value;
+    }
+    corruptionInput.addEventListener('input', corruptionChange);
+    corruptionInput.addEventListener('change', corruptionChange);
+
+    const speedChange = () => {
+		speed = speedInput.value;
+		speedDisplay.innerText = speedInput.value;
+    }
+    speedInput.addEventListener('input', speedChange);
+    speedInput.addEventListener('change', speedChange);
+
+
+    const textChange = () => {
+    	result.innerText = textInput.value.toLowerCase();
+    }
+	textInput.addEventListener('input', textChange);
+    result.innerText = textInput.value.toLowerCase();
+
 
     function frameLoop() {
-        result.innerText = corruptString(textInput.value.toLowerCase(), corruption);
+        result.innerText = corruptString(result.innerText, corruption);
         setTimeout(frameLoop, 1000/speed);
     }
     frameLoop();
 });
-
